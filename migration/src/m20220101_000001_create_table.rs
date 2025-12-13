452e7b8eb18ffd8d2558361f6ded7889c9e04dc8
Vehan Hemsara <contact.vehanhemsara@gmail.com>
2025-12-13 00:54:45 +0000

    feat: add migration module with initial migration scripts and configuration

---
+++ migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,46 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Users::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Users::Id))
+                    .col(string(Users::Email).unique_key().not_null())
+                    .col(string(Users::PasswordHash).not_null())
+                    .col(
+                        ColumnDef::new(Users::CreatedAt)
+                            .timestamp()
+                            .not_null()
+                            .extra("DEFAULT now()"),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        manager
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+    Email,
+    PasswordHash,
+    CreatedAt,
+}
